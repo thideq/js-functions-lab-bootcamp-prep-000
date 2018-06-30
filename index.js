f21430c9d847b6223b47a4b4c2d666545f752ad5
Thierry Dequin <thierrydequin@hotmail.com>
2018-06-30 23:03:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `happy ${Holidays}, ${name}!`
 }
+function holidayCountdown(holiday,days) {
+  return `It's ${days} days until ${holiday}!`
+}
