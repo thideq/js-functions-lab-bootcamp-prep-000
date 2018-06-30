68122112f10f814c0242c45f1de832e481d3889f
Thierry Dequin <thierrydequin@hotmail.com>
2018-06-30 23:05:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 function happyHolidays() {
-  return `happy Holidays!`
+  return `Happy Holidays!`
 }
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
